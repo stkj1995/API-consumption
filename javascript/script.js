be19d020eaa74dd949f948630e752aa988763888
Sophie <soph1155@stud.kea.dk>
2025-09-29 14:55:38 +0200

    insert API

--- javascript/script.js
+++ javascript/script.js
@@ -1,4 +1,4 @@
-const API_KEY = "YOUR_API_KEY_HERE"; // ⬅️ insert your TMDB API key
+const API_KEY = "0bea372fe8eac0d8890fc859457d2a6d";
 const BASE_URL = "https://api.themoviedb.org/3";
 const IMG_URL = "https://image.tmdb.org/t/p/w500";
 
